febfdc7a994121295d991ea5cfe730c4522f7f17
Nathaniel Leonard <31514598+NateAnthonyLeonard@users.noreply.github.com>
2020-09-16 23:46:26 -0400

    File Upload of Project
    Assignment 2! Latest update to project.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Nathaniel Leonard on 9/16/20.
+//  Copyright © 2020 Nathaniel Leonard. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Nathaniel Leonard on 9/16/20.
+//  Copyright © 2020 Nathaniel Leonard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
